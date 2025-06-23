aa94412c2ea6a1e038d2cf899c23db4b16100849
Nathan Traxler <nathantraxler@MacBook-Pro.local>
2025-06-22 19:25:31 -0700

    Change text background color

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,18 +1,18 @@
 
 export default function NavHeader() {
   return (
-    <header className="flex items-center justify-around bg-[var(--color-bg-header)]">
-      <div className="flex-2 text-4xl font-bold px-8 py-4">
+    <header className="flex items-center justify-around bg-(--color-bg-header)">
+      <div className="flex-2 text-4xl font-bold px-8 py-4 text-(--text)">
         Nathan Traxler
       </div>
       <nav className="flex-1 flex justify-around">
-        <a href="/blog" className="text-xl font-medium hover:opacity-70">
+        <a href="/blog" className="text-xl font-medium hover:opacity-70 text-(--text)">
           Blog
         </a>
-        <a href="/resume" className="text-xl font-medium hover:opacity-70">
+        <a href="/resume" className="text-xl font-medium hover:opacity-70 text-(--text)">
           Resume
         </a>
-        <a href="/contact" className="text-xl font-medium hover:opacity-70">
+        <a href="/contact" className="text-xl font-medium hover:opacity-70 text-(--text)">
           Contact
         </a>
       </nav>
